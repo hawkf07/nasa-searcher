e0422407a2de489084ccf5ade9eac62b0fcabed3
fikri <fikrifadilah299@gmail.com>
2022-08-02 13:58:41 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_NASA_API_KEY)
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
